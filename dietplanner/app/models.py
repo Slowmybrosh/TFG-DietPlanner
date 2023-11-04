1bfb3dd157213f2820bbc5c7fed9f49cdb62ce6c
Alejandro Olivares <a.olivaresdelrey.pierres@gmail.com>
2023-11-04 20:30:45 +0100

    Advances #90. Se migra la base de datos

--- dietplanner/app/models.py
+++ dietplanner/app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Ingredients(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = 'ingredient'
+
+class Recipes(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    steps = models.TextField()
+    number_of_ingredients = models.IntegerField(null=True)
+
+    class Meta:
+        db_table = 'recipes'
+
+class RecipeIngredient(models.Model):
+    recipe_id = models.ForeignKey(Recipes, on_delete=models.CASCADE)
+    ingredient_id = models.ForeignKey(Ingredients, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'recipeingredient'
+        constraints = [
+            models.UniqueConstraint(fields=['recipe_id', 'ingredient_id'], name='unique_recipe_ingredient'),
+        ]
+
