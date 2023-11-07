be164bd1fc4ce5fc8b724fb95f7d7494e8b4b4cc
Alejandro Olivares <a.olivaresdelrey.pierres@gmail.com>
2023-11-07 18:36:11 +0100

    Advances #45. Se cambian los test al comportamiento del usuario

--- dietplanner/app/tests/tests.py
+++ dietplanner/app/tests/tests.py
@@ -1,34 +1,56 @@
-import sys, unittest, warnings, pytest
+import sys, unittest, pytest
+from django.test import RequestFactory, TestCase
+from django.urls import reverse
 from app.buscador import Buscador
+from ..views import *
+from ..models import *
 
-class TestBuscador(unittest.TestCase):
+class TestBuscador(TestCase):
 
     def setUp(self) -> None:
         self.buscador = Buscador()
+        self.factory = RequestFactory()
+        recipe1 = Recipes(name="test1", steps="test1")
+        recipe2 = Recipes(name="test2", steps="test2")
+        recipe3 = Recipes(name="test3", steps="test3")
+        recipe4 = Recipes(name="test4", steps="test3")
+        recipe5 = Recipes(name="test5", steps="test5")
+        recipe1.save()
+        recipe2.save()
+        recipe3.save()
+        recipe4.save()
+        recipe5.save()
+        ingredient1 = Ingredients(id=0,name="test1")
+        ingredient1.save()
+        contains = RecipeIngredient(recipe_id=recipe2,ingredient_id=ingredient1)
+        contains.save()
 
-    # Buscador
-    @pytest.mark.django_db
-    def test_buscar_ingrediente_id(self):
-        assert self.buscador.buscarIngredienteID(0) == (0, 'bacalao')
+    def test_call_view_index(self):
+        response = self.client.get(reverse("index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'buscador.html')
+    
+    def test_call_view_resultados_no_exists(self):
+        response = self.client.get(reverse("resultados"), follow=True)
+        self.assertRedirects(response, '/')
 
-    @pytest.mark.django_db
-    def test_buscar_ingrediente_nombre(self):
-        assert self.buscador.buscarIngredienteNombre('bacalao') == [(0, 'bacalao')]
+    def test_call_view_resultados(self):
+        self.client.cookies['IDs'] = []
+        response = self.client.post(reverse("resultados"),data={'metodo': 'ingredientes'})
+        self.assertEqual(response.status_code,200)
+        self.assertTemplateUsed(response, 'resultados.html')
 
-    @pytest.mark.django_db
-    def test_buscar_receta_id(self):
-        resultado = self.buscador.buscarRecetasID(1)
-        expected = (1,'Budín de bacalao con espinacas', 'Paso 1\n\nSe le quita al bacalao la piel y se seca con papel absorvente;Paso 2\n\nEn el vaso de la batidora se echa una clara de huevo, el bacalao y la nata con la sal, pimienta y nuez moscada. Se añada las espinacas y el limón. Cuidado con la sal y el bacalao.;Paso 3\n\nSe mete en un recipiente apto para Air fire engrasado con mantequilla. Se calienta la Air fire 2 minutos antes. Después se pone a 180 grados 15 minutos. Sino está poner unos minutos mas.', 7, ['bacalao', 'huevo', 'espinacas', 'nata', 'limón', 'queso en polvo', 'mantequilla'])
-        assert resultado == expected
+    def test_call_view_guardadas(self):
+        request = self.client.get(reverse("guardadas"))
+        self.assertEqual(request.status_code, 200)
+        self.assertTemplateUsed(request, 'resultados.html')
 
-    @pytest.mark.django_db
-    def test_buscar_receta_nombre(self):
-        resultado = self.buscador.buscarRecetasNombre('bacalao')
-        expected = [(1, 'Budín de bacalao con espinacas', ['bacalao', 'huevo', 'espinacas', 'nata', 'limón', 'queso en polvo', 'mantequilla']), (9, 'Bacalao con salsa de calabacín y langostinos con 2 texturas', ['bacalao', 'huevo', 'cebolla', 'ajo', 'harina', 'langostinos', 'calabazín', 'mejillones', 'caldo de verduras', 'hierbas provenzales', 'queso philadelpia']), (23, 'Potaje de garbanzos, bacalao y gambones salvajes (versión exprés)', ['bacalao', 'cebolla', 'ajo', 'pimentón', 'caldo de verduras', 'AOVE', 'pimiento verde', 'comino', 'vino blanco', 'caldo de pescado', 'garbanzos', 'gambones', 'pisto'])]
-        assert resultado == expected
+    def test_call_view_detalle(self):
+        request = self.client.get(reverse("detalle_receta",args="1"))
+        self.assertEqual(request.status_code, 200)
+        self.assertTemplateUsed(request, 'receta.html')
 
-    @pytest.mark.django_db
-    def test_buscar_receta_ingredentes(self):
-        resultado = self.buscador.buscarRecetasIngredientes([0,1,2,3,5,6,7])
-        expected = [(1, 'Budín de bacalao con espinacas', ['bacalao', 'huevo', 'espinacas', 'nata', 'limón', 'queso en polvo', 'mantequilla'])]
-        assert resultado == expected
+    def test_data_in_detalle(self):
+        request = self.client.get(reverse("detalle_receta",args="1"))
+        self.assertTemplateUsed(request, 'receta.html')
+        self.assertEqual(request.context['receta_id'], 1)
--- dietplanner/app/views.py
+++ dietplanner/app/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
-from django.http import HttpResponse
 from .buscador import Buscador
 import json, re
 
@@ -37,19 +36,30 @@ def resultados(request):
     """
     if request.method == 'POST':
         metodo = request.POST.get("metodo")
-        guardadas_json = json.loads(request.COOKIES.get("recetas"))
-        guardadas  = [eval(i) for i in guardadas_json]
+        if request.COOKIES.get("recetas"):
+            guardadas_json = json.loads(request.COOKIES.get("recetas"))
+            guardadas  = [eval(i) for i in guardadas_json]
+        else:
+            guardadas = []
         if metodo == "ingredientes":
-            seleccionados_json = json.loads(request.COOKIES.get("IDs"))
-            IDs = list(map(int, seleccionados_json["seleccionados"]))
-            lista_recetas = buscador.buscarRecetasIngredientes(IDs)
-            recetas = []
-            for receta in lista_recetas:
-                recetas.append((receta[0],receta[1],receta[2],receta[0] in guardadas))
-            context = {
-                "top": buscador.getRandom(5),
-                "resultados": recetas
-            }
+            if request.COOKIES.get("IDs"):
+                seleccionados_json = json.loads(request.COOKIES.get("IDs"))
+                if seleccionados_json:
+                    IDs = list(map(int, seleccionados_json["seleccionados"]))
+                    lista_recetas = buscador.buscarRecetasIngredientes(IDs)
+                    recetas = []
+                    for receta in lista_recetas:
+                        recetas.append((receta[0],receta[1],receta[2],receta[0] in guardadas))
+                    context = {
+                        "top": buscador.getRandom(5),
+                        "resultados": recetas
+                    }
+                else:
+                    context = {
+                        "top": buscador.getRandom(5)
+                    }
+            else:
+                return redirect("index")
         if metodo == "recetas":
             query = request.POST.get('search-recetas')
             lista_recetas = buscador.buscarRecetasNombre(query)
@@ -61,27 +71,32 @@ def resultados(request):
                 "resultados": recetas
             }
         return render(request,'resultados.html',context)
+    else:
+        return redirect("index")
     
 def detalle_receta(request, id):
     """
     Endpoint para ver los detalles de una receta
     """
-    receta = buscador.buscarRecetasID(id)
-    formateado = re.sub("Paso (\d+)\n\n","",receta[2]).split(";")
-    IDs = []
-    if request.COOKIES.get("recetas"):
-        guardadas_json = json.loads(request.COOKIES.get("recetas"))
-        IDs  = [eval(i) for i in guardadas_json]
-    
-    saved = id in IDs
-    context = {
-        "receta_id": receta[0],
-        "receta_nombre": receta[1],
-        "receta_pasos": formateado,
-        "receta_ingredientes": receta[4],
-        "saved": saved
-    }
-    return render(request,'receta.html', context)
+    try:
+        receta = buscador.buscarRecetasID(id)
+        formateado = re.sub("Paso (\d+)\n\n","",receta[2]).split(";")
+        IDs = []
+        if request.COOKIES.get("recetas"):
+            guardadas_json = json.loads(request.COOKIES.get("recetas"))
+            IDs  = [eval(i) for i in guardadas_json]
+        
+        saved = id in IDs
+        context = {
+            "receta_id": receta[0],
+            "receta_nombre": receta[1],
+            "receta_pasos": formateado,
+            "receta_ingredientes": receta[4],
+            "saved": saved
+        }
+        return render(request,'receta.html', context)
+    except:
+        return redirect("index")
 
 def guardadas(request):
     """
